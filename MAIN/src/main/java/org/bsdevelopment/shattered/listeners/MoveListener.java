f622daca825ca0dca17ec8a6eed8fa45ce6ff58b
brainsynder <briansnyder498@gmail.com>
2022-10-19 01:00:12 -0400

    Added GRAY_WOOL as a spectator block when the player walks on it

--- MAIN/src/main/java/org/bsdevelopment/shattered/listeners/MoveListener.java
+++ MAIN/src/main/java/org/bsdevelopment/shattered/listeners/MoveListener.java
@@ -4,11 +4,15 @@ import com.jeff_media.morepersistentdatatypes.DataType;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.bsdevelopment.shattered.game.GameState;
 import org.bsdevelopment.shattered.game.ShatteredPlayer;
 import org.bsdevelopment.shattered.managers.Management;
+import org.bsdevelopment.shattered.managers.list.GameManager;
 import org.bsdevelopment.shattered.option.Option;
 import org.bukkit.FluidCollisionMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,5 +35,25 @@ public class MoveListener implements Listener {
         if (option.getDescription() == null) return;
 
         event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD+option.getDescription()));
+    }
+
+    @EventHandler
+    public void onMove (PlayerMoveEvent event) {
+        ShatteredPlayer shatteredPlayer = Management.PLAYER_MANAGER.getShatteredPlayer(event.getPlayer());
+        if (shatteredPlayer.getState() != ShatteredPlayer.PlayerState.LOBBY) return;
+
+
+        if (event.getTo().getBlock().getRelative(BlockFace.DOWN).getType() == Material.GRAY_WOOL) {
+            if (shatteredPlayer.isPlaying()
+                    || shatteredPlayer.isSpectating()
+                    || (Management.GAME_MANAGER.getCurrentGamemode() == null)
+                    ||  (Management.GAME_MANAGER.getState() != GameState.IN_GAME)) {
+                event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatColor.RED + "" + ChatColor.BOLD+"UNABLE TO SPECTATE GAME"));
+                return;
+            }
+
+            Management.GAME_MANAGER.joinGame(shatteredPlayer, GameManager.Reason.LOGIN_OR_OUT);
+            shatteredPlayer.setSpectating(true);
+        }
     }
 }
