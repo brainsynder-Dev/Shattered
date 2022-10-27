210554a97446114ce2478c550e764931a1dc54f5
brainsynder <briansnyder498@gmail.com>
2022-10-26 22:44:39 -0400

    Fixed issue when a player leaves that was in a ready cube

--- MAIN/src/main/java/org/bsdevelopment/shattered/listeners/JoinLeaveListener.java
+++ MAIN/src/main/java/org/bsdevelopment/shattered/listeners/JoinLeaveListener.java
@@ -1,9 +1,11 @@
 package org.bsdevelopment.shattered.listeners;
 
 import org.bsdevelopment.shattered.Shattered;
+import org.bsdevelopment.shattered.game.GameState;
 import org.bsdevelopment.shattered.game.ShatteredPlayer;
 import org.bsdevelopment.shattered.managers.Management;
 import org.bsdevelopment.shattered.managers.list.GameManager;
+import org.bsdevelopment.shattered.managers.list.lobby.ReadyCube;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -32,12 +34,26 @@ public class JoinLeaveListener implements Listener {
     @EventHandler
     private void onQuit(PlayerQuitEvent event) {
         ShatteredPlayer shatteredPlayer = Management.PLAYER_MANAGER.getShatteredPlayer(event.getPlayer());
-        Management.GAME_MANAGER.leaveGame(shatteredPlayer, GameManager.Reason.LOGIN_OR_OUT);
+        handleLeave(shatteredPlayer);
     }
 
     @EventHandler
     private void onKick(PlayerKickEvent event) {
         ShatteredPlayer shatteredPlayer = Management.PLAYER_MANAGER.getShatteredPlayer(event.getPlayer());
+        handleLeave(shatteredPlayer);
+    }
+
+    private void handleLeave (ShatteredPlayer shatteredPlayer) {
         Management.GAME_MANAGER.leaveGame(shatteredPlayer, GameManager.Reason.LOGIN_OR_OUT);
+
+        if (Management.GAME_MANAGER.getState() != GameState.WAITING) return;
+
+        ReadyCube readyCube1 = Management.LOBBY_MANAGER.getReadyCube1();
+        if (readyCube1.isReady() && readyCube1.getCubePlayers().isEmpty())
+            readyCube1.toggleCube(false);
+
+        ReadyCube readyCube2 = Management.LOBBY_MANAGER.getReadyCube2();
+        if (readyCube2.isReady() && readyCube2.getCubePlayers().isEmpty())
+            readyCube2.toggleCube(false);
     }
 }
